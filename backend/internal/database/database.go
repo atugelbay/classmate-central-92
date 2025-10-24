1afd264592d96e1fca6285dd98d5a594b15df93c
atugelbay <aldiyar.tugelbay@gmail.com>
2025-10-24 21:20:50 +0500

    Add DATABASE_URL support for Railway deployment

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -14,15 +14,21 @@ type Database struct {
 }
 
 func NewDatabase() (*Database, error) {
-	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	// Try to use DATABASE_URL first (Railway, Heroku, etc.)
+	connStr := os.Getenv("DATABASE_URL")
+	
+	// If DATABASE_URL is not set, build connection string from individual env vars
+	if connStr == "" {
+		connStr = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_SSLMODE"),
+		)
+	}
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
